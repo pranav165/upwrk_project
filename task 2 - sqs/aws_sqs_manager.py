b7b1b92a7338578ea81763722fc2e357abc488ce
Pranav Sharma <pranav165@gmail.com>
2023-01-10 13:35:40 +0530

    converted to class

--- task 2 - sqs/aws_sqs_manager.py
+++ task 2 - sqs/aws_sqs_manager.py
@@ -8,66 +8,66 @@ import requests
 
 requests.packages.urllib3.disable_warnings()
 
-client = boto3.client('sqs', aws_access_key_id='',
-                      aws_secret_access_key='',
-                      region_name='eu-west-2', verify=False)
-queue_url = 'https://sqs.eu-west-2.amazonaws.com/565045766653/BALAG711171VK9GP_SYS.fifo'
+
+class SqsClient:
+    def __init__(self, queue_url):
+        self.queue_url = queue_url
+        self.client = boto3.client('sqs', aws_access_key_id='',
+                                   aws_secret_access_key='',
+                                   region_name='us-west-2', verify=False)
+
+    def send_message(self, result=None):
+        payload = {
+            "timeSlotId": "1234",
+            "date": date.today().strftime("%d/%m/%Y"),
+            "datetimeOfTest": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        }
+
+        if result == 1:
+            payload["result"] = "accepted"
+        elif result == 2:
+            payload["result"] = "rejected"
+        self.client.send_message(
+            QueueUrl=self.queue_url,
+            MessageBody=json.dumps(payload),
+            MessageGroupId="test"
+        )
+        print("SENDER ------> Sending Message with payload as {}".format(payload))
+
+    @retry(Exception, tries=4, delay=2)
+    def receive_message(self):
+        response = self.client.receive_message(
+            QueueUrl=self.queue_url,
+            AttributeNames=['All']
+        )
+        print("RECEIVER ------> Received a message with payload {}".format(response['Messages'][0]['Body']))
+
+        receipt_handle = response['Messages'][0]['ReceiptHandle']
+        self.delete_message(receipt_handle)
+
+    def process_message(self):
+        self.receive_message()
+        answer = int(input("Enter 1 to select 2 to reject \n"))
+        if answer == 1:
+            print("Accepted")
+            self.send_message(1)
+        elif answer == 2:
+            print("Rejected")
+            self.send_message(2)
+        else:
+            print("Wrong input")
+
+    def delete_message(self, receipt_handle):
+        self.client.delete_message(
+            QueueUrl=self.queue_url,
+            ReceiptHandle=receipt_handle
+        )
+
+    #
 
 
-def send_message(id, result=None):
-    payload = {
-        "timeSlotId": id,
-        "date": date.today().strftime("%d/%m/%Y"),
-        "datetimeOfTest": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    }
-
-    if result == 1:
-        payload["result"] = "accepted"
-    elif result == 2:
-        payload["result"] = "rejected"
-    client.send_message(
-        QueueUrl=queue_url,
-        MessageBody=json.dumps(payload),
-        MessageGroupId=id
-    )
-    print("SENDER ------> Sending Message with payload as {}".format(payload))
-
-
-@retry(Exception, tries=4, delay=2)
-def receive_message():
-    response = client.receive_message(
-        QueueUrl=queue_url,
-        AttributeNames=['All']
-    )
-    print("RECEIVER ------> Received a message with payload {}".format(response['Messages'][0]['Body']))
-
-    receipt_handle = response['Messages'][0]['ReceiptHandle']
-    delete_message(receipt_handle)
-
-
-def process_message(id):
-    receive_message()
-    answer = int(input("Enter 1 to select 2 to reject"))
-    if answer == 1:
-        print("\n Accepted")
-        send_message(id, 1)
-    elif answer == 2:
-        print("\n Rejected")
-        send_message(id, 2)
-    else:
-        print("Wrong input")
-
-
-def delete_message(receipt_handle):
-    client.delete_message(
-        QueueUrl=queue_url,
-        ReceiptHandle=receipt_handle
-    )
-
-
-#
+sqs = SqsClient(queue_url='https://sqs.us-west-2.amazonaws.com/643068144265/testpranav.fifo')
 while True:
-    id = str(uuid.uuid1())
-    send_message(id)
-    process_message(id)
-    receive_message()
+    sqs.send_message()
+    sqs.process_message()
+    sqs.receive_message()
